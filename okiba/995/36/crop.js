63b83ff5935ad834307f56bdde9144f67af76097
e-ll-c <0000000ell@gmail.com>
2021-08-30 10:47:12 +0900

    画像の誤字治ってなかった
    マジでこれで最後で頼む～

--- okiba/995/36/crop.js
+++ okiba/995/36/crop.js
@@ -1,23 +1,36 @@
 import sharp from 'sharp'
 import fs from 'fs'
 import path from 'path'
+import { Command } from 'commander/esm.mjs'
 
+
+const program = new Command()
+program
+  .option('--dpr <dpr>', 'dpr', 1)
+  .parse(process.argv)
+const dpr = program.opts().dpr
 const sizes =JSON.parse(fs.readFileSync('grid.json', 'utf-8'))
-const image = sharp('all-dpr2.png')
+const image = sharp(`all-dpr${dpr}.png`)
+
 
 sizes.forEach(e => {
-  image.extract({ width: e.width * 2, height: (e.height - 5) * 2, left: 0, top: (e.top + 5) * 2 })
-    .toFile(path.join('text-dpr2', e.name))
-    .then(info => {
-      //console.log(info)
-    })
-    .catch(err => {
-      console.log(e.name)
-      console.log(err)
-    })
+  image.extract({
+    width: e.width * dpr,
+    height: (e.height - 5) * dpr,
+    left: 0,
+    top: (e.top + 5) * dpr,
+  })
+  .toFile(path.join(`text-dpr${dpr}`, e.name))
+  .then(info => {
+    //console.log(info)
+  })
+  .catch(err => {
+    console.log(e.name)
+    console.log(err)
+  })
 })
 
 
 sizes.forEach(e => {
-  console.log(`img ${e.width} ${e.height - 5} https://e-ll-c.github.io/okiba/995/36/text-dpr2/${e.name}`)
+  console.log(`img ${e.width} ${e.height - 5} https://e-ll-c.github.io/okiba/995/36/${e.name}`)
 })
